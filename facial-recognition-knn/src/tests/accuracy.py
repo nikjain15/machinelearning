c57163912943dac7f02b13fa64473d7de8ef5746
nikjain15 <161231855+nikjain15@users.noreply.github.com>
2024-03-26 18:42:48 -0400

    Update accuracy.py

--- facial-recognition-knn/src/tests/accuracy.py
+++ facial-recognition-knn/src/tests/accuracy.py
@@ -1,1 +1,18 @@
+import numpy as np
+
+def accuracy(truth, preds):
+    """
+    Computes the accuracy of predicted labels.
+
+    Args:
+    - truth: An array of true labels.
+    - preds: An array of predicted labels.
+
+    Returns:
+    - The accuracy as a float.
+    """
+    truth = np.array(truth).flatten()
+    preds = np.array(preds).flatten()
+    
+    return np.mean(truth == preds)
 
