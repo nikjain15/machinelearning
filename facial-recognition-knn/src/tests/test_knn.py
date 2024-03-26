12915392274011dbbeda813c39e56990d8303d61
nikjain15 <161231855+nikjain15@users.noreply.github.com>
2024-03-26 18:49:47 -0400

    Update test_knn.py

--- facial-recognition-knn/src/tests/test_knn.py
+++ facial-recognition-knn/src/tests/test_knn.py
@@ -1,1 +1,18 @@
+import unittest
+import numpy as np
+from src.knn import findknn
+from src.accuracy import accuracy
+
+class KNNTest(unittest.TestCase):
+    def test_findknn(self):
+        # Your test cases here
+        pass
+
+class AccuracyTest(unittest.TestCase):
+    def test_accuracy(self):
+        # Your test cases here
+        self.assertAlmostEqual(accuracy([1, 2, 1, 2], [1, 2, 1, 1]), 0.75)
+
+if __name__ == '__main__':
+    unittest.main()
 
